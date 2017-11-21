24afc3bf6843de8305114296224989f2adac62e7
Jessica <johmert@users.noreply.github.com>
2017-11-20 16:40:51 -0800

    added access getAccessToken method

--- jammming/src/util/Spotify.js
+++ jammming/src/util/Spotify.js
@@ -1,5 +1,4 @@
 const clientID ='e7cbf631fd03446bb66a5678bb9587df';
-const secret ='dfdd520c5434494b8fc1c2149d82372b';
 const redirectURI = 'http://localhost:3005/';
 
 let accessToken;
@@ -10,6 +9,19 @@ const Spotify = {
       return accessToken;
     }
     //adding in accessToken info
+    const token = window.location.href.match(/access_token=([^&]*)/);
+    const expireTime = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (token && expireTime) {
+      accessToken=token[1];
+      const expiresIn = Number(expireTime[1]);
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+      window.location = accessUrl
+    }
   },
 
   search(term) {
